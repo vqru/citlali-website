0009570a4db22ae65812b01dd7d63ffe8f3cb2d7
vqru <154732468+vqru@users.noreply.github.com>
2026-04-14 11:40:28 +0530

    Update votes.js

--- api/votes.js
+++ api/votes.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
     try {
-        const r = await fetch('http://noel.hidencloud.com:24684/api/votes');
+        const r = await fetch('http://zeus.hidencloud.com:24662/api/votes');
         const data = await r.json();
         res.setHeader('Access-Control-Allow-Origin', '*');
         res.json(data);
