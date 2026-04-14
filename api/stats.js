e4a8a84d244668506d3f17b24de794ad79356fea
vqru <154732468+vqru@users.noreply.github.com>
2026-04-14 11:39:55 +0530

    Update stats.js

--- api/stats.js
+++ api/stats.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
     try {
-        const r = await fetch('http://noel.hidencloud.com:24684/api/stats');
+        const r = await fetch('http://zeus.hidencloud.com:24662/api/stats');
         const data = await r.json();
         res.setHeader('Access-Control-Allow-Origin', '*');
         res.json(data);
