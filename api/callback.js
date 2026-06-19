9be70df09063df933e553d30f8e443bea5133e08
vqru <154732468+vqru@users.noreply.github.com>
2026-06-19 14:23:30 +0530

    Update callback.js

--- api/callback.js
+++ api/callback.js
@@ -49,16 +49,17 @@ module.exports = async (req, res) => {
 		await connectDb()
 
 		// Step 1: exchange code for short-lived token
-		const tokenRes = await fetch('https://graph.facebook.com/v21.0/oauth/access_token', {
-			method:  'POST',
-			headers: { 'Content-Type': 'application/json' },
-			body:    JSON.stringify({
-				client_id:     process.env.META_APP_ID,
-				client_secret: process.env.META_APP_SECRET,
-				grant_type:    'authorization_code',
-				redirect_uri:  process.env.OAUTH_REDIRECT_URI,
-				code
-			})
+		const tokenParams = new URLSearchParams({
+			client_id:     process.env.META_APP_ID,
+			client_secret: process.env.META_APP_SECRET,
+			grant_type:    'authorization_code',
+			redirect_uri:  process.env.OAUTH_REDIRECT_URI,
+			code
+		})
+
+		const tokenRes = await fetch('https://api.instagram.com/oauth/access_token', {
+			method: 'POST',
+			body:   tokenParams
 		})
 
 		const tokenData = await tokenRes.json()
